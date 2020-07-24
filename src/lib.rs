aa0a62d3a9cff7aa9d3b171ba5e541c6b1201f33
Sophie 'Sharky' Schumann <sharky@sharky.pw>
2020-07-24 13:53:49 +0200

    working init! 🦈

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+use anyhow::Result;
+use serde::Deserialize;
+use regex::Regex;
+
+const DHL_JSON_REGEXP: &str = r#".*initialState: JSON\.parse\((.+")\).*"#;
+
+mod deserialize_as_thingie {
+    use serde_json;
+    use serde::de::{Deserialize, DeserializeOwned, Deserializer};
+    
+    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+    where
+        T: DeserializeOwned,
+        D: Deserializer<'de>,
+    {
+        use serde::de::Error;
+        let j = String::deserialize(deserializer)?;
+        serde_json::from_str(&j).map_err(Error::custom)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DHLPackageItemHistoryEvent {
+    #[serde(rename = "datum")]
+    pub date: String,
+    #[serde(rename = "status")]
+    pub status: String,
+    #[serde(rename = "ruecksendung")]
+    pub return_shipment: bool,
+    #[serde(rename = "ort")]
+    pub location: Option<String>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DHLPackageItemHistory {
+    #[serde(rename = "events")]
+    pub events: Vec<DHLPackageItemHistoryEvent>,
+    #[serde(rename = "aktuellerStatus")]
+    pub current_status: String,
+    #[serde(rename = "fortschritt")]
+    pub steps: u64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DHLPackageItemDetails {
+    #[serde(rename = "sendungsverlauf")]
+    pub history: DHLPackageItemHistory,
+    #[serde(rename = "zielland")]
+    pub destination_country: String,
+}
+#[derive(Deserialize, Debug)]
+pub struct DHLPackageItem {
+    #[serde(with = "deserialize_as_thingie")]
+    pub id: u64,
+    
+    #[serde(rename = "hasCompleteDetails")]
+    pub has_complete_details: bool,
+
+    #[serde(rename = "sendungsdetails")]
+    pub item_details: DHLPackageItemDetails,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DHLPackageStatus {
+    #[serde(rename = "sendungen")]
+    pub items: Vec<DHLPackageItem>
+}
+
+pub fn find_and_derez_json(body: &str) -> Result<DHLPackageStatus> {
+    let rex = Regex::new(DHL_JSON_REGEXP).unwrap();
+
+    let caps = rex.captures(&body).unwrap();
+
+    let json_escaped = &caps[1];
+    let json = json_escaped
+        .trim()
+        [1..json_escaped.len()-1]
+        .replace(r#"\""#, "\"");
+    
+    //println!("{}", json);
+
+    let r: DHLPackageStatus = serde_json::from_str(&json).unwrap();
+    Ok(r)
+}
+
+pub async fn get_dhl_package_status(package_id: &str) -> Result<DHLPackageStatus, anyhow::Error> {
+    let my_url = format!("https://www.dhl.de/int-verfolgen/?lang=en&domain=de&lang=en&domain=de&lang=en&domain=de&lang=en&domain=de&piececode={}", package_id);
+    let body = reqwest::get(&my_url)
+        .await?
+        .text().await?;
+
+    find_and_derez_json(&body)
+}
+
+pub fn get_dhl_package_status_from_str(body: &str) -> Result<DHLPackageStatus,anyhow::Error> {
+    find_and_derez_json(&body)
+}
---
+++ tests/lib.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_derez() {
+
+        const EXAMPLE_BODY: &str = r#"
+        <script>
+  
+    window.__INITIAL_APP_STATE__ = {
+      initialState: JSON.parse("{\"sendungen\":[{\"id\":\"523361125086\",\"hasCompleteDetails\":true,\"sendungsinfo\":{\"gesuchteSendungsnummer\":\"523361125086\"},\"sendungsdetails\":{\"sendungsnummern\":{\"sendungsnummer\":\"523361125086\"},\"eigenschaften\":{},\"sendungsverlauf\":{\"datumAktuellerStatus\":\"2020-07-24T02:19:00+02:00\",\"aktuellerStatus\":\"Shipment sent directly from parcel center to business customer\",\"kurzStatus\":\"The shipment has been sent directly from the parcel center to the business customer.\",\"fortschritt\":5,\"farbe\":0,\"iconId\":\"5\",\"events\":[{\"datum\":\"2020-07-22T16:39:00+02:00\",\"status\":\"The shipment has been taken from the PACKSTATION for onward transportation\",\"ruecksendung\":false},{\"datum\":\"2020-07-23T15:39:00+02:00\",\"ort\":\"Hannover\",\"status\":\"The shipment has been processed in the parcel center\",\"ruecksendung\":false},{\"datum\":\"2020-07-23T15:40:00+02:00\",\"status\":\"Shipment sent directly from parcel center to business customer\",\"ruecksendung\":false},{\"datum\":\"2020-07-24T02:18:00+02:00\",\"ort\":\"Neumark\",\"status\":\"The shipment has been processed in the parcel center\",\"ruecksendung\":false},{\"datum\":\"2020-07-24T02:19:00+02:00\",\"status\":\"Shipment sent directly from parcel center to business customer\",\"ruecksendung\":false}]},\"services\":{\"statusbenachrichtigung\":{\"aktuellerStatus\":true,\"geplanteZustellung\":false,\"erfolgteZustellung\":false}},\"zustellung\":{\"showAbholcode\":false,\"abholcodeAvailable\":false,\"zugestelltAnPackstation\":false,\"benachrichtigtInFiliale\":false},\"zielland\":\"Germany\",\"istZugestellt\":true,\"ruecksendung\":false,\"retoure\":false,\"warenpost\":false,\"expressSendung\":false},\"versandDatumBenoetigt\":false}]}"),
+      config: {"verfolgenDataPath":"/int-verfolgen/data","verfolgenContextPath":"/int-verfolgen","shipperIconPath":"/int-verfolgen/shippericons","assetPath":"/int-verfolgen/static/v900/","verfolgenVersion":"v900","currentDomain":"de","currentLanguage":"en","verfolgenCsrfToken":"56f42818-45bb-4275-adcc-4fa2be3fa41e","verfolgenBundleHash":"c6a995e6e77dd9599daa2b190b11e2c41ad2c847d17129301f456cd15cf72abb","verfolgenI18NHash":"5a9eeb116d2d22eb8683adf1d0b23d9ffd88a3f760e430074811fb02c739e5cf","portal":true,"zeroPercentPackageNotificationActive":true,"frontendDebug":false,"initialWG":0,"liveTrackingRefreshTime":1,"shipperIconClickedRefreshTime":10,"detailsViewNavigationRefreshTime":60,"renderMode":"WIDGET","showBanner":false,"packstationApiKey":"a0d5b9049ba8918871e6e20bd5c49974","searchLimit":10}
+    };
+    window.nol = {};
+    window.nol.redirectRules = [{check: function (data) {             if (data.parameter && data.parameter.piececode) {               var shipmentId = data.parameter.piececode;               return Boolean(shipmentId.match(/(LX|LY|RX|RS)[0-9]{9}DE/gi));             }             return false;},destination: function (data) {return 'https://www.packet.deutschepost.com/web/portal-europe/packet_traceit?barcode=' +  data.parameter.piececode}}];
+  
+</script>
+"#;
+
+        let res = dhl_tracker::find_and_derez_json(&EXAMPLE_BODY).unwrap();
+        let item = res.items.first().unwrap();
+        println!("{:?}", item);
+
+        assert_eq!(item.id, 523361125086);
+        assert_eq!(item.has_complete_details, true);
+        
+        let item_details = &item.item_details;
+        assert_eq!(item_details.destination_country, "Germany");
+        let history = &item_details.history;
+        assert_eq!(history.steps, 5);
+        let events = &history.events;
+        assert_ne!(events.len(), 0);
+        assert_eq!(events.first().unwrap().return_shipment, false);
+    }
+}
